feb0626043f13da87e96fbfd9647c1c0f9a0777f
stephanekouam10 <stephane.kouam237@gmail.com>
2022-09-08 20:22:57 +0200

    HomeScreen and Details integration

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,13 +1,88 @@
 import React from "react";
-import { Text } from 'react-native'
+import { Text, View, StyleSheet, ScrollView, Image } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { SafeAreaView } from "react-native";
+import COLORS from "../consts/colors";
+import { SecondaryButton } from "../components/Button";
 
-const DetailsScreen = ({navigation}) => {
+const DetailsScreen = ({navigation, route}) => {
+    const item = route.params;
+    console.log(item)
     return (
-        <SafeAreaView>
-            <Text>OnBoardScreen</Text>
+        <SafeAreaView style={{backgroundColor: COLORS.white}}>
+            <View style={styles.header}>
+                <Icon name="arrow-back-ios" size={28} onPress={navigation.goBack} />
+                <Text style={{fontSize: 20, fontWeight: 'bold'}}>Details</Text>
+            </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                  style={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: 280,
+                  }}>
+                    <Image source={item.image} style={{height: 220, width: 220}} />
+                </View>
+                <View style={styles.details}>
+                    <View 
+                      style={{
+                        flexDirection: 'row', 
+                        justifyContent: 'space-between', 
+                        alignItems: 'center'}}>
+                            <Text 
+                                style={{fontSize: 25, fontWeight: 'bold', color: COLORS.white}}>
+                                {item.name}
+                            </Text>
+                            <View style={styles.iconContainer}>
+                                <Icon name='favorite-border' color={COLORS.primary} size={25} />
+                            </View>
+                           
+                    </View>
+                    <Text style={styles.detailsText}>
+                        Sit magna cillum veniam tempor labore sit enim amet velit Lorem. 
+                        Dolor ad reprehenderit commodo esse ipsum irure voluptate cupidatat 
+                        eiusmod aliquip Lorem pariatur amet. Velit et ea anim sint consequat ipsum. 
+                        Voluptate duis sunt nisi eu excepteur et nostrud ut dolor culpa et. Occaecat 
+                    </Text>
+                    <View style={{marginTop: 40, marginBottom: 40}}>
+                        <SecondaryButton title={"Add to cart"}/>
+                    </View>
+
+                   
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    header: {
+        paddingVertical: 20,
+        flexDirection: 'row',
+        paddingHorizontal: 20
+    },
+    details: {
+        paddingHorizontal: 20,
+        paddingTop: 40,
+        paddingBottom: 60,
+        backgroundColor: COLORS.primary,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30
+    },
+    iconContainer: {
+        backgroundColor: COLORS.white,
+        height: 50,
+        width: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 30
+    },
+    detailsText: {
+        marginTop: 10,
+        lineHeight: 22,
+        fontSize: 16,
+        color: COLORS.white,
+    }
+});
 
 export default DetailsScreen;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,13 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { auth } from '../firebase'
-import { useNavigation } from '@react-navigation/native'
+import { StyleSheet, Text, TouchableOpacity, View, SafeAreaView, Image, TextInput, ScrollView, FlatList, Dimensions, TouchableHighlight } from 'react-native'
+import React from 'react';
+import { auth } from '../firebase';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useNavigation } from '@react-navigation/native';
+import COLORS from '../consts/colors';
+import categories from '../consts/categories';
+import foods from '../consts/foods';
+const {width} = Dimensions.get("screen");
+const cardWidth = width/2 - 20;
 
 const HomeScreen = () => {
   
@@ -15,10 +21,124 @@ const HomeScreen = () => {
         })
         .catch(error => alert(error.message))
     }
+
+
+    const [selectedCategoryIndex,  setSelectedCategoryIndex] = React.useState(0);
+    const ListCategories = () => {
+        return ( 
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={styles.categoriesListContainer}>
+                {categories.map((category, index) => (
+                    <TouchableOpacity key={index} activeOpacity={0.8} onPress={()=>setSelectedCategoryIndex(index)}>
+                        <View style={{
+                            backgroundColor:selectedCategoryIndex == index
+                            ? COLORS.primary
+                            : COLORS.secondary,
+                            ...styles.categoryBtn}}>
+                                <View style={styles.categoryBtnImgCon}>
+                                    <Image 
+                                      source={category.image}
+                                      style={{height: 35, width: 35, resizeMode: 'cover'}} 
+                                    />
+                                </View>
+                                <Text style={{
+                                    fontSize: 15, 
+                                    fontWeight: 'bold', 
+                                    marginLeft: 10,
+                                    color: selectedCategoryIndex == index ? COLORS.white : COLORS.primary}}>
+                                        {category.name}
+                                </Text>
+                            </View>
+                    </TouchableOpacity>
+
+                ))}
+            </ScrollView>
+        );
+    };
+
+    const Card = ({food}) => {
+        return (
+            <TouchableHighlight 
+            underlayColor={COLORS.white} 
+            activeOpacity={0.9} 
+            onPress={()=>navigation.navigate('DetailsScreen', food)}>
+                <View style={styles.Card}>
+                    <View style={{alignItems: 'center',  top: -40}}>
+                        <Image source={food.image} style={{height: 120, width: 120}}/>
+                    </View>
+                    <View style={{marginHorizontal: 20}}>
+                        <Text style={{fontSize: 18, fontWeight: 'bold'}}>{food.name}</Text>
+                        <Text style={{fontSize: 14, color: COLORS.grey, marginTop: 2}}>{food.ingredients}</Text>
+                    </View>
+                    <View 
+                    style={{marginTop: 5, marginHorizontal: 20, 
+                    flexDirection:'row', justifyContent: "space-between"}}>
+                        <Text style={{fontSize: 18, fontWeight: 'bold'}}>€ {food.price}</Text>
+                        <View style={styles.addToCardBtn}>
+                            <Icon name="add" size={20} color={COLORS.white} />
+                        </View>
+                    </View>
+                </View>
+            </TouchableHighlight>
+        );
+    };
+   
   
   
     return (
-    <View style={styles.container}>
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+            <View style={styles.header}>
+                <View>
+                    <View style={{flexDirection: 'row'}}>
+                        <Text style={{fontSize: 28}}>Hello,</Text>
+                        <Text style={{fontSize: 28, fontWeight: 'bold', marginLeft: 10}}>Stephane</Text>
+                    </View>
+                    <Text style={{marginTop: 10, fontSize: 22, color: COLORS.grey}}>
+                        What do you want today
+                    </Text>
+                </View>
+                <Image source={require('../assets/person.png')} style={{height: 50, width: 50, borderRadius: 25}} />
+            </View>
+            <View
+                style={{
+                    marginTop: 40,
+                    flexDirection: 'row',
+                    paddingHorizontal: 20
+                }}>
+                    <View style={styles.inputcontainer}>
+                        <Icon name="search" size={28} />
+                        <TextInput 
+                          style={{flex: 1, fontSize: 18}}
+                          placeholder= "Search for food"
+                        />
+                    </View>
+                    <View style={styles.sortBtn}>
+                        <Icon name="tune" size={28} color={COLORS.white} />
+                    </View>
+            </View>
+            <View>
+                <ListCategories />
+            </View>
+            <FlatList 
+              showsVerticalScrollIndicator={false}
+              numColumns={2}
+              data={foods}
+              renderItem={({item}) => <Card food={item} />}
+            />
+        </SafeAreaView>
+
+
+
+
+
+    )
+
+
+
+
+    {/* <View style={styles.container}>
       <Text>Email: {auth.currentUser?.email} </Text>
       <TouchableOpacity
         onPress={handleSignOut}
@@ -26,8 +146,8 @@ const HomeScreen = () => {
       >
         <Text style={styles.buttonText}>Sign out</Text>
       </TouchableOpacity>
-    </View>
-  )
+    </View> */}
+  
 }
 
 export default HomeScreen
@@ -38,6 +158,71 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
+    header: {
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+    },
+    inputcontainer: {
+        flex: 1,
+        height: 50,
+        borderRadius: 10,
+        flexDirection: 'row',
+        backgroundColor: COLORS.light,
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    sortBtn: {
+        width: 50,
+        height: 50,
+        marginLeft: 10,
+        backgroundColor: COLORS.primary,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    categoriesListContainer: {
+        paddingVertical: 30,
+        alignItems: 'center',
+        paddingHorizontal: 20
+    },
+    categoryBtn: {
+        height: 45,
+        width: 120,
+        marginRight: 7,
+        borderRadius: 30,
+        alignItems: 'center',
+        paddingHorizontal: 5,
+        flexDirection: 'row'
+    },
+    categoryBtnImgCon: {
+        height: 35,
+        width: 35,
+        backgroundColor: COLORS.white,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    Card: {
+        height: 220,
+        width: cardWidth,
+        marginHorizontal: 10,
+        marginBottom: 20,
+        marginTop: 50,
+        borderRadius: 15,
+        elevation: 13,
+        backgroundColor: COLORS.secondary
+    },
+    addToCardBtn: {
+        height: 30,
+        width: 30,
+        borderRadius: 20,
+        backgroundColor: COLORS.primary,
+        justifyContent:'center',
+        alignItems: 'center',
+    },
+
     button: {
         backgroundColor: '#0782F9',
         width: '60%',
